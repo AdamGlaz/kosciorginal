c4bdad6d6ab9b880371a87146ed83cc56bef892e
Adam Wlaź <wlazadam6@gmail.com>
2024-12-10 13:23:17 +0100

    Dodaj pliki projektów.

---
+++ kosciorginal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kosciorginal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kosciorginal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace kosciorginal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
